ff4b2fdd11b622bd81eb777dd388209beefbfc31
My Name <muskan@instarem.com>
2021-06-03 23:58:46 +0530

    added webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.export = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel_loader",
+        },
+      },
+    ],
+  },
+};
