68a4064e57c2eb30fc47c35ad23faa74cda1dc62
My Name <muskan@instarem.com>
2021-06-08 23:12:32 +0530

    button file created

---
+++ src/js/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Button extends Component {
+  render() {
+    return (
+      <div>
+        <button>Reusable Button</button>
+      </div>
+    );
+  }
+}
